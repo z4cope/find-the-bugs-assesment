2f13f553f3e5fa7008a6edcd8469810cb072f8a1
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-14 15:15:14 +0200

    Add js code

---
+++ index.js
@@ -0,0 +1,13 @@
+// Please solve the current issues
+// 1- container background should be black
+// 2- heading color should white
+// 3- field margin isn't working
+// discover the bugs and solve them
+
+const container = document.getElementsByClassName("container");
+const heading = document.getElementsByTagName("h2");
+const field = document.getElementsById("input");
+
+container.style.backgroundColor = "#000";
+heading.style.color = "#fff";
+field.style.margin = "2rem";
